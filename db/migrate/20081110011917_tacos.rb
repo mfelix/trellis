aade9e57e27f51c772a6746fd18180d8b4e7fb7c
Michael Felix <michael@revelationglobal.com>
2009-03-24 09:44:58 -0700

    fixing tacos

--- db/migrate/20081110011917_tacos.rb
+++ db/migrate/20081110011917_tacos.rb
@@ -1,9 +1,9 @@
 class Tacos < ActiveRecord::Migration
   def self.up
-    add_column :items, :collection_id, :integer 
+    #add_column :items, :collection_id, :integer 
   end
 
   def self.down
-    remove_column :items, :collection_id, :integer 
+    #remove_column :items, :collection_id, :integer 
   end
 end
