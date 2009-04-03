3f268c1f60dbfcaf64c24cbed8d45ea6e52e5007
Michael Felix <michael@revelationglobal.com>
2009-04-03 02:30:55 -0700

    VISUALSSSSs/s/s/s/

--- public/javascripts/tiles.js
+++ public/javascripts/tiles.js
@@ -1,14 +1,54 @@
 $(document).ready(function(){
 
+  jQuery.delegate = function(rules) {
+    return function(e) {
+      var target = $(e.target);
+      for (var selector in rules)
+        if (target.is(selector)) return rules[selector].apply(this, $.makeArray(arguments));
+    }
+  }
+
+
   $(".tile").hover(
       function () {
         $(this).find(".action").show();
+        $(this).addClass("hover");
+        $(this).find(".microtile").addClass("hover");
       }, 
       function () {
        $(this).find(".action").hide();
+        $(this).removeClass("hover");
       }
     );
-  
-  
 
+  $(".micro").hover(
+      function () {
+        $(this).addClass("hover");
+      }, 
+      function () {
+        $(this).removeClass("hover");
+      }
+    );
+
+    $("#head").hover(
+        function () {
+          $(this).addClass("hover");
+        }, 
+        function () {
+          $(this).removeClass("hover");
+        }
+      );
+        
+    $(".tile").click(function () {
+      $(".tile").fadeOut("fast");
+      window.location.href = $(this).find(".linker").attr("href");
+    });
+
+  
+    $('.container').click($.delegate({
+      '#head, #head-id': function() { history.go(-1); },
+      '.container': function() {  },
+      '.edit': function() { /* do edit stuff */ }
+    }));
+    
 });
